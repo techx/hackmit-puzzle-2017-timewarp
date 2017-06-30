5af3cca040af7e38acb8f87dafbb3f4c3e9e4356
Patrick Insinger <patrickinsinger@gmail.com>
2017-06-29 23:27:24 -0400

    chill

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uwsgi --gevent 100 --http :8000 --wsgi-file app.py --callable app --static-map /suchsecret=jars
+uwsgi --gevent 2 --http :8000 --wsgi-file app.py --callable app --static-map /suchsecret=jars
